782e00015593a7a1cb7e64598065d3e93e97b90a
KosmicKisses <silvery.kosmic.kisses@gmail.com>
2022-03-11 16:21:52 -0800

    Working on Main Character Index
    Added content and formatting

--- mainCharacters.php
+++ mainCharacters.php
@@ -1,27 +1,39 @@
 <?php require 'pageStart.php'; ?>
   <title>Home - Out of Amospia</title>
-	<div class="center">
+  <link rel="stylesheet" href="indexStyles.css">
+	<div class="center" style="background-color: #9076FF;">
 		<div class="main-content">
 			<div class="row">
 				<div class="col-md-3 m-2">
-					<div style="padding-bottom:100%; overflow:hidden; position:relative">
+					<div class="indexImg">
 						<img src="images/rayImage1.png" style="position: absolute;width: 100%;">
 					</div>
 				</div>
-				<div class="col-md-8 sideBox">
-					<p>Ray</p>
+				<div class="col-md-8 Box">
+					<p class="name">Ray</p>
 					<p>Ray is an ideal me. Brash, confident, adventurous.</p>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-8 sideBox">
-					<p>Kosi</p>
-					<p>More reservd</p>
+				<div class="col-md-8 Box" style="text-align:right;">
+					<p class="name">Kosi</p>
+					<p>More reserved and feminine. Got a mild rebellious streak. Usually has controlling parents.</p>
 				</div>
 				<div class="col-md-3 m-2">
-					<div style="padding-bottom:100%; overflow:hidden; position:relative">
+					<div class="indexImg">
+						<img src="images/kosiPFP.png" style="position: absolute;width: 100%;">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 m-2">
+					<div class="indexImg">
 						<img src="images/rayImage1.png" style="position: absolute;width: 100%;">
 					</div>
+				</div>
+				<div class="col-md-8 Box">
+					<p class="name">Amber</p>
+					<p>Cheerful, a bit naive. Difficult past but doesn't let it get to her.</p>
 				</div>
 			</div>
 		</div>
