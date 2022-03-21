04ceb68aebcc3a7df32c43f2aaecb30d3d70abd5
KosmicKisses <silvery.kosmic.kisses@gmail.com>
2022-03-21 14:34:07 -0700

    Added character template

---
+++ profileBase.php
@@ -0,0 +1,133 @@
+<?php require 'pageStart.php'; ?>
+  <title>Profile Template</title>
+  <link rel="stylesheet" href="stylesRayProfile.css">
+  <div class="center">
+    <div class="main-content">
+      <div class="wrapper">
+        <div class="one element">
+        </div>
+        <div class="two element">
+
+          <div style="display: -webkit-flex;display: flex;margin: .5rem 0 1.5rem 0;">
+            <i class="fas fa-quote-left" style="flex: 1 1;text-align: center;"></i>
+            <i style="letter-spacing: 1px;text-align: center;flex: 5 2;">This is where a quote goes</i>
+            <i class="fas fa-quote-right" style="flex: 1 1;text-align: center;"></i>
+          </div>
+          <div id="about">
+            <div class="pr-md-1 mb-3" id="uno">
+              <i class="fas fa-user"></i>
+              First and Last name
+            </div>
+            <div class="pl-md-1 mb-3" id="dos">
+              <i class="fas fa-comment-dots"></i>
+              Nickname
+            </div>
+            <div class="pr-md-1 mb-3" id="tres">
+              <i class="fas fa-birthday-cake"></i>
+              Age or age range
+            </div>
+            <div class="pl-md-1 mb-3" id="quatro">
+              <i class="fas fa-calendar-alt"></i>
+              Birthday
+            </div>
+            <div class="pr-md-1 mb-3" id="cinco">
+              <i class="fas fa-venus-mars"></i>
+              Gender
+            </div>
+            <div class="pl-md-1 mb-3" id="seis">
+              <i class="fas fa-heartbeat"></i>
+              Orientation
+            </div>
+            <div class="pr-md-1 mb-3" id="siete">
+              <i class="fas fa-ruler"></i>
+              5' 10"
+            </div>
+            <div class="l-md-1 mb-3" id="ocho">
+              <i class="fas fa-question"></i>
+              Race/Ethnicity
+            </div>
+            <div class="pr-md-1 mb-3" id="nueve">
+              <i class="fas fa-book"></i>
+              Occupation
+            </div>
+            <div class="pl-md-1 mb-3" id="diez">
+              <i class="fas fa-balance-scale"></i>
+              Alignment
+            </div>
+            <div class="pr-md-1 mb-3" id="once">
+              <i class="fas fa-volume-up"></i>
+              <a href="https://youtu.be/1k8craCGpgs" target="_BLANK" style="text-decoration: none;color: #14171A;">
+                <i class="fas fa-play" style="margin-left: 1.5em;"></i>
+              </a>
+            </div>
+            <div class="pr-md-1 mb-3" id="trece">
+              <i class="fas fa-star"></i>
+              Personality type
+            </div>
+            <div class="pl-md-1 mb-3" id="doce">
+              <i class="fas fa-code"></i>
+              <a href="/Eggy" style="color: #999;">Eggy</a>
+            </div>
+            <div class="pl-md-1 mb-3" id="catorce">
+              <i class="fas fa-hands"></i>
+              Designer
+            </div>
+          </div>
+        </div>
+        <div class="three element" style="padding: .5em;">
+          <div style="display: -webkit-flex;display: flex;height: 100%;">
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #7F152A;"></div>
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #959595;"></div>
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #51006F;"></div>
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #0500FF;"></div>
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #5A00A9;"></div>
+            <div style="-webkit-flex: 1 1;flex: 1 1;background-color: #F800EA;"></div>
+          </div>
+        </div>
+        <div class="four element">
+          <ul class="list-unstyled">
+            <li><i class="fas fa-check"></i> Like 1</li>
+            <li><i class="fas fa-check"></i> Like 2/li>
+            <li><i class="fas fa-check"></i> Like 3</li>
+            <li><i class="fas fa-check"></i> Like 4</li>
+            <li><i class="fas fa-check"></i> Like 5</li>
+          </ul>
+        </div>
+        <div class="five element">
+          <ul class="list-unstyled">
+            <li><i class="fas fa-times"></i> Dislike 1</li>
+            <li><i class="fas fa-times"></i> Dislike 2</li>
+            <li><i class="fas fa-times"></i> Dislike 3</li>
+            <li><i class="fas fa-times"></i> Dislike 4</li>
+            <li><i class="fas fa-times"></i> Dislike 5</li>
+          </ul>
+        </div>
+        <div class="six element">
+          <img src="images/">
+        </div>
+        <div class="seven element">
+          Seven
+        </div>
+        <div class="eight element">
+          Eight
+        </div>
+        <div class="nine element">
+          Nine
+        </div>
+        <div class="ten element">
+          Ten
+        </div>
+        <div class="eleven element">
+          Songs: Stronger (What Doesn’t Kill You) – Kelly Clarkson, Don’t Stop Believing – Journey, Save the World – Swedish House Mafia, We are the Champions – Queen, 15. Just Like Fire – P!nk
+        </div>
+        <div class="twelve element">
+          Twelve
+        </div>
+        <div class="thirteen element">
+          Thirteen
+        </div>
+      </div>
+    </div>  
+  </div>
+  <?php require 'raySide.php'; ?>
+<?php require 'pageEnd.php'; ?>
