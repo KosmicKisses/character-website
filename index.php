208946d3073305d7b3bca2ca15eb91ea6c1aa381
KosmicKisses <silvery.kosmic.kisses@gmail.com>
2022-03-21 14:34:47 -0700

    Edited sidebar
    Replaced placeholder with main characters

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 			<p><a href="https://en.wikipedia.org/wiki/Haikyu!!">Haikyu!!</a></p>
 		</div>
 		<div class="sideBox">
-			<p>Worlds</p>
-			<p><a href="https://en.wikipedia.org/wiki/One_Piece">One Piece</a></p>
-			<p><a href="https://www.pokemon.com/us/">Pokemon</a></p>
-			<p><a href="https://en.wikipedia.org/wiki/Haikyu!!">Haikyu!!</a></p>
+			<p>Main Character</p>
+			<p><a href="https://out.of.amospia.com">Ray</a></p>
+			<p><a href="https://out.of.amospia.com">Kosi</a></p>
+			<p><a href="https://out.of.amospia.com">Amber</a></p>
 		</div>
 	</div>
 <?php require 'pageEnd.php'; ?>
