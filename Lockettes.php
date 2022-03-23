c06cfce73a0035e0fe598a37ea79f5a5eb87713b
KosmicKisses <silvery.kosmic.kisses@gmail.com>
2022-03-22 20:27:42 -0700

    Create Lockettes.php
    Made a simple placeholder page for all the Lockette characters

---
+++ Lockettes.php
@@ -0,0 +1,20 @@
+<?php require 'pageStart.php'; ?>
+  <title>Lockettes</title>
+	<div class="center">
+		<div class="main-content">
+			<p><a href="https://toyhou.se/~world/58710.lockettes">Toyhouse Link</a></p>
+		</div>	
+	</div>
+	<div class="sideBar">
+		<div class="sideBox">
+			<p>Characters</p>
+			<p><a href="ash-social-page/feed.php">Ash</a></p>
+			<p><a href="https://toyhou.se/12106119.lilly">Lilly</a></p>
+		</div>
+		<div class="sideBox">
+			<p>Other Closed Species</p>
+			<p><a href="https://toyhou.se/~world/12910.pakkeli-cluster">Pakkeli-Cluster</a></p>
+			<p><a href="https://www.deviantart.com/dextroluma/journal/Dextroluma-Species-lore-514987927">Dextroluma</a></p>
+		</div>
+	</div>
+<?php require 'pageEnd.php'; ?>
