d3e0021ddc9323c34a5c3d86cc3667e682d842a0
KosmicKisses <silvery.kosmic.kisses@gmail.com>
2022-03-22 20:44:58 -0700

    Modifying Main Character side bar
    Adding actual content to the side bar on the Main Character page

--- mainCharacters.php
+++ mainCharacters.php
@@ -37,30 +37,35 @@
 				</div>
 			</div>
 			<div class="row">
+				<div class="col-md-8 Box">
+					<p class="name">Amber</p>
+					<p>Cheerful, a bit naive. Difficult past but doesn't let it get to her.</p>
+				</div>
 				<div class="col-md-3 m-2">
 					<div class="indexImg">
 						<img src="iimages/amberPFP.png" style="position: absolute;width: 100%;">
 					</div>
-				</div>
-				<div class="col-md-8 Box">
-					<p class="name">Amber</p>
-					<p>Cheerful, a bit naive. Difficult past but doesn't let it get to her.</p>
 				</div>
 			</div>
 		</div>
 	</div>	
 	<div class="sideBar">
 		<div class="sideBox">
-			<p>Worlds</p>
-			<p><a href="https://en.wikipedia.org/wiki/One_Piece">One Piece</a></p>
-			<p><a href="https://www.pokemon.com/us/">Pokemon</a></p>
-			<p><a href="https://en.wikipedia.org/wiki/Haikyu!!">Haikyu!!</a></p>
+			<p class="sideTitle"><a href="https://en.wikipedia.org/wiki/One_Piece">One Piece</a></p>
+			<p class="sideSubtitle">Ray</p>
+			<p><i>Shipped with Nico Robin</i></p>
+			<p class="sideSubtitle">Kosi</p>
+			<p><i>Shipped with Brooke</i></p>
+			<p class="sideSubtitle">Ashton</p>
+			<p><i>Shipped with Trafalger Law</i></p>
+			<p class="sideSubtitle">Zina</p>
 		</div>
 		<div class="sideBox">
-			<p>Worlds</p>
-			<p><a href="https://en.wikipedia.org/wiki/One_Piece">One Piece</a></p>
-			<p><a href="https://www.pokemon.com/us/">Pokemon</a></p>
-			<p><a href="https://en.wikipedia.org/wiki/Haikyu!!">Haikyu!!</a></p>
+			<p class="sideTitle"><a href="https://www.pokemon.com/us/">Pokemon</a></p>
+			<p class="sideSubtitle">Ray</p>
+			<p><i>Shipped with Elesa, Skyla, and Malva</i></p>
+			<p class="sideSubtitle">Kosi</p>
+			<p><i>Shipped with Nessa and Sonia</i></p>
 		</div>
 	</div>
 <?php require 'pageEnd.php'; ?>
